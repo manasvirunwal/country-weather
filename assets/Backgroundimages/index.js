3e9a4859cdfcf317cde02b52b4a3c50e02a23047
manasvirunwal <manasvirunwal@gmail.com>
2021-08-29 10:50:35 +0530

    initial commit message

---
+++ assets/Backgroundimages/index.js
@@ -0,0 +1,6 @@
+const haze = require('./img/haze.jpg');
+const sunny = require('./img/sunny.jpg');
+const snow = require('./img/snow.jpg');
+const rainy = require('./img/rainy.jpg');
+
+export {haze, sunny, snow, rainy };
